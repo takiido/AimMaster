f68f32aea5623d0322d58fb01184db6d7ef12913
takiido <157808651+takiido@users.noreply.github.com>
2024-08-05 21:51:47 -0500

    Created game preset class and game presets based on it

---
+++ Assets/Scripts/GamePresets.cs
@@ -0,0 +1,10 @@
+// Copyright takiido. All Rights Reserved
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Presets/New Preset", fileName = "New Game Preset", order = 1)]
+public class GamePresets : ScriptableObject
+{
+    public float inchesPer360;
+    public float fov;
+}
